04459e16f2542efe9f430883705720f9f1f5d7fb
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-03-26 04:47:54 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let Random: number = Math.floor(Math.random() * 100 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userGuessNumber",
+    type: "number",
+    message: "Guess a number between 0 and 100",
+  },
+]);
+
+if (answer.userGuessNumber == Random) {
+  console.log("You guessed correctly you win!");
+} else if (answer.userGuessNumber > 100) {
+  console.log("Your guess is not between 0 and 100");
+} else {
+  console.log("Your guess is not correct!");
+}
